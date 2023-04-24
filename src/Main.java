8b03850737bad739b507babf420e8c63609beb48
Zhecka <bondarenkoyy@gmail.com>
2023-04-24 15:42:49 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args)  {
+        Scanner reader = new Scanner((System.in));
         ArrayList<String> list = new ArrayList<String>();
 
         for(int i = 0; i < 3; i++){
-            list.add(reader.readLine());
+            list.add(reader.nextLine());
         }
 
         list.remove(2);
